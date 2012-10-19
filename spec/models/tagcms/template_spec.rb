44ff62b56151a134d5d2cd95f0b9ad3b7d82cdaf
Tim Glen <tim@tag.ca>
2012-10-19 17:09:22 -0400

    updated to better factory names

--- spec/models/tagcms/template_spec.rb
+++ spec/models/tagcms/template_spec.rb
@@ -9,10 +9,11 @@ describe Tagcms::Template do
   it { should ensure_inclusion_of(:template_type).in_array(Tagcms::Template.types) }
   it { should_not validate_presence_of(:content) }
   context "on update" do
-    subject { FactoryGirl.create(:tagcms_template) }
+    subject { FactoryGirl.create(:template) }
     it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of :content }
   end
 
   it { should have_many :pages }
+  it { should have_many :containers }
 end
