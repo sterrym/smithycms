34497ac96a8afaf1cf724b5e282c4b68c2ee940f
Tim Glen <tim@tag.ca>
2012-10-25 17:00:25 -0400

    started the UI for content blocks and templates

---
+++ app/controllers/smithy/content_blocks_controller.rb
@@ -0,0 +1,50 @@
+require_dependency "smithy/application_controller"
+
+module Smithy
+  class ContentBlocksController < ApplicationController
+    before_filter :load_content_blocks
+    respond_to :html, :json
+
+    def index
+      respond_with @content_blocks
+    end
+
+    def new
+      @content_block = ContentBlock.new(params[:content_block])
+      respond_with @content_block
+    end
+
+    def create
+      @content_block = ContentBlock.new(params[:content_block])
+      @content_block.save
+      flash.notice = "Your content_block was created" if @content_block.persisted?
+      respond_with @content_block do |format|
+        format.html { redirect_to [:edit, @content_block] }
+      end
+    end
+
+    def edit
+      @content_block = ContentBlock.find(params[:id])
+      respond_with @content_block
+    end
+
+    def update
+      @content_block = ContentBlock.find(params[:id])
+      flash.notice = "Your content_block was saved" if @content_block.update_attributes(params[:content_block])
+      respond_with @content_block do |format|
+        format.html { redirect_to [:edit, @content_block] }
+      end
+    end
+
+    def destroy
+      @content_block = ContentBlock.find(params[:id])
+      @content_block.destroy
+      respond_with @content_block
+    end
+
+    private
+      def load_content_blocks
+        @content_blocks = ContentBlock.all
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Smithy::Engine.routes.draw do
   scope "/smithy" do
+    resources :content_blocks
     resources :pages
     resources :templates
     resources :settings
