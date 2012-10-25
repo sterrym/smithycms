644303722a4514c89feea36054913e5a17193400
Tim Glen <tim@tag.ca>
2012-10-25 10:03:51 -0400

    The Page controller

---
+++ app/controllers/smithy/pages_controller.rb
@@ -0,0 +1,61 @@
+require_dependency "smithy/application_controller"
+
+module Smithy
+  class PagesController < ApplicationController
+    before_filter :initialize_page, :only => [ :new, :create ]
+    before_filter :load_page, :only => [ :edit, :update, :destroy ]
+    before_filter :load_parent, :except => [ :index ]
+    before_filter :load_root, :only => [ :index ]
+    respond_to :html, :json
+
+    def index
+      respond_with @root
+    end
+
+    def new
+      respond_with @page
+    end
+
+    def create
+      @page.save
+      flash.notice = "Your page was created" if @page.persisted?
+      respond_with @page do |format|
+        format.html { edit_page_path(@page.id) }
+      end
+    end
+
+    def edit
+      respond_with @page
+    end
+
+    def update
+      flash.notice = "Your page was saved" if @page.update_attributes(params[:page])
+      respond_with @page do |format|
+        format.html { redirect_to edit_page_path(@page.id) }
+      end
+    end
+
+    def destroy
+      @page.destroy
+      respond_with @page
+    end
+
+    private
+      def initialize_page
+        @page = Page.new(params[:page])
+      end
+
+      def load_page
+        @page = Page.find(params[:id])
+        @root = @page if Page.root == @page
+      end
+
+      def load_parent
+        @parent = @page.parent
+      end
+
+      def load_root
+        @root = Page.root
+      end
+  end
+end
