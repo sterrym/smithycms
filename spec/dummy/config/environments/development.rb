da471a359d6c51f4ebbedf8c9a9b326cafea231c
Tim Glen <tim@tag.ca>
2012-10-25 09:55:42 -0400

    added letter_opener and live-reload to the test app

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -1,6 +1,9 @@
 Dummy::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # allow the browser to live-reload when a view is changed
+  config.middleware.insert_before(Rack::Lock, Rack::LiveReload)
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -15,6 +18,9 @@ Dummy::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+
+  # Open emails in the browser
+  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
