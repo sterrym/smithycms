d749d8a95237607ca6964694a0908d68917e87cf
Tim Glen <tim@tag.ca>
2012-10-25 16:58:40 -0400

    nested form helpers

---
+++ app/assets/javascripts/smithy/nested_forms.js
@@ -0,0 +1,57 @@
+/*
+  Nested Forms
+*/
+$(function() {
+  $('form a.add_nested_fields').live('click', function() {
+    // Setup
+    var assoc   = $(this).attr('data-association');           // Name of child
+    var content = $('#' + assoc + '_fields_blueprint').html(); // Fields template
+
+    // Make the context correct by replacing new_<parents> with the generated ID
+    // of each of the parent objects
+    var context = ($(this).parents('.fields').children('input:first').attr('name') || '').replace(new RegExp('[[a-z]+]$'), '');
+
+    // context will be something like this for a brand new form:
+    // project[tasks_attributes][1255929127459][assignments_attributes][1255929128105]
+    // or for an edit form:
+    // project[tasks_attributes][0][assignments_attributes][1]
+    if(context) {
+      var parent_names = context.match(/[a-z]+_attributes/g) || []
+      var parent_ids   = context.match(/[0-9]+/g)
+
+      for(i = 0; i < parent_names.length; i++) {
+        if(parent_ids[i]) {
+          content = content.replace(
+            new RegExp('(\[' + parent_names[i] + '\])\[.+?\]', 'g'),
+            '$1[' + parent_ids[i] + ']'
+          )
+        }
+      }
+    }
+
+    // Make a unique ID for the new child
+    var regexp  = new RegExp('new_' + assoc, 'g');
+    var new_id  = new Date().getTime();
+    content     = content.replace(regexp, new_id)
+
+    $(this).parent().after(content);
+    return false;
+  });
+
+  $('form a.remove_nested_fields').live('click', function() {
+    var container = $(this).closest('.destroy');
+    var hidden_field = $('input[type=hidden]', container).val('1');
+    $(this).closest('.nested').hide();
+    return false;
+  });
+
+  // replace the remover checkboxes with a link
+  $('form .destroy > label').hide();
+  $('form .destroy .controls label').each(function(i) {
+    link = $('<a href="javascript:void(0);" class="btn btn-danger btn-mini">Delete</a>').addClass("remove_nested_fields");
+    $(this).empty().append($(link));
+    // hidden_field = $("input[type=hidden]", $(this).parent("li"));
+    // $(this).empty().append($(hidden_field)).append($(link));
+  });
+
+});
--- app/helpers/smithy/application_helper.rb
+++ app/helpers/smithy/application_helper.rb
@@ -3,5 +3,22 @@ module Smithy
     def project_title
 
     end
+
+    def link_to_add_fields(name, association, form_builder)
+      @fields ||= {}
+      model_object = form_builder.object.class.reflect_on_association(association).klass.new
+      form_builder.template.concat(raw %Q[<div id="#{association}_fields_blueprint" style="display:none;">])
+      fields = form_builder.semantic_fields_for(association, model_object, :child_index => "new_#{association}") do |builder|
+        form_builder.template.concat(raw render(association.to_s.singularize + "_fields", :f => builder))
+      end
+      form_builder.template.concat(fields)
+      form_builder.template.concat(raw '</div>')
+      form_builder.template.link_to(name, "javascript:void(0)", :class => "add_nested_fields", "data-association" => association)
+    end
+
+    def link_to_remove_fields(name, form_builder)
+      form_builder.hidden_field(:_destroy) + @template.link_to(name, "javascript:void(0)", :class => "remove_nested_fields")
+    end
+
   end
 end
