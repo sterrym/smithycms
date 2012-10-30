a8c59215e35a7fbd69f9a06bc1f82cc6a533300d
Tim Glen <tim@tag.ca>
2012-10-30 16:27:49 -0400

    added routes for page content and content

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Smithy::Engine.routes.draw do
   scope "/smithy" do
+    # CMS admin
     resources :content_blocks
-    resources :pages
+    resources :pages do
+      resources :contents, :controller => "PageContents",
+        :only => [ :new, :create, :edit, :update, :destroy ]
+    end
     resources :templates
     resources :settings
+
+    # Content Blocks
+    resources :contents, :except => :index
   end
   # match '*path', :to => 'pages#show'
 end
