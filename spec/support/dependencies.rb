5ac6fd99b8b4947fdcf7f9c750b276b37ca5ecd7
Tim Glen <tim@tag.ca>
2012-11-14 14:09:11 -0500

    added capybara integration for "feature" specs

--- spec/support/dependencies.rb
+++ spec/support/dependencies.rb
@@ -1,6 +1,19 @@
+require 'capybara/rails'
+require 'capybara/rspec'
 require 'factory_girl_rails'
 require 'faker'
 require 'fakeweb'
 require 'shoulda-matchers'
 
 Fog.mock! # this mocks out all AWS calls - really nice
+
+# set up focus ability and include all of our route url helpers
+RSpec.configure do |config|
+  # Filter specs to only run focus specs
+  config.filter_run :focus => true
+  # if none are focused, run everything
+  config.run_all_when_everything_filtered = true
+
+  config.include Smithy::Engine.routes.url_helpers
+  config.include Rails.application.routes.url_helpers
+end
