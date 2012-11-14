e7e0d885cbe0bf2d2a646807f01c424e6d1f678b
Tim Glen <tim@tag.ca>
2012-11-14 15:38:15 -0500

    only load up settings if the table exists

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,5 @@
-%w(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_S3_BUCKET).each do |k|
-  ENV[k] = Smithy::Setting.find_by_name(k).value if Smithy::Setting.find_by_name(k) && ENV[k].blank?
+if ActiveRecord::Base.connection.table_exists? "smithy_settings"
+  %w(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_S3_BUCKET).each do |k|
+    ENV[k] = Smithy::Setting.find_by_name(k).value if Smithy::Setting.find_by_name(k) && ENV[k].blank?
+  end
 end
