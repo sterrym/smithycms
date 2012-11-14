de81ef2160eaac1848e22bb476c2b5201ae900da
Tim Glen <tim@tag.ca>
2012-11-14 14:13:28 -0500

    use a scope

--- lib/smithy/liquid/database.rb
+++ lib/smithy/liquid/database.rb
@@ -2,7 +2,7 @@ module Smithy
   module Liquid
     class Database
       def read_template_file(template_path, context)
-        _include = Smithy::Template.where(:template_type => 'include').where(:name => template_path).first
+        _include = Smithy::Template.partials.where(:name => template_path).first
         raise ActiveRecord::RecordNotFound, "No such template '#{template_path}'" unless _include.present?
 
         _include.content
