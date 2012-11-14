cb5ffbf2dc913b28ce1d648af2553a260630999c
Tim Glen <tim@tag.ca>
2012-11-13 20:33:37 -0500

    added some sane defaults

--- spec/factories/smithy_pages.rb
+++ spec/factories/smithy_pages.rb
@@ -4,5 +4,7 @@ FactoryGirl.define do
   factory :page, :class => 'Smithy::Page' do
     template
     title { Faker::Lorem.words(2).join(' ') }
+    show_in_navigation true
+    published_at 1.day.ago
   end
 end
