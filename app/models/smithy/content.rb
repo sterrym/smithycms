e4b8f330bd3c51d65c11afaf573c23f9de793edd
Tim Glen <tim@tag.ca>
2012-11-15 17:12:56 -0500

    fo

--- app/models/smithy/content.rb
+++ app/models/smithy/content.rb
@@ -1,9 +1,8 @@
 module Smithy
   class Content < ActiveRecord::Base
+    include Smithy::ContentBlocks::Model
     attr_accessible :content
 
     validates_presence_of :content
-
-    has_many :page_contents, :as => :content_block
   end
 end
