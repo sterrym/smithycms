c94583bf5bd898977d4819b421156f1f9bafaba6
Tim Glen <tim@tag.ca>
2012-11-17 11:13:49 -0600

    simplified the defaults

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,34 +20,19 @@ Spork.prefork do
   Dir[File.join(File.dirname(__FILE__), 'support', '**', '*.rb')].each { |f| require f }
 
   RSpec.configure do |config|
-    # ## Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
     config.use_transactional_fixtures = true
-
-    # If true, the base class of anonymous controllers will be inferred
-    # automatically. This will be the default behavior in future versions of
-    # rspec-rails.
+    # config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.infer_base_class_for_anonymous_controllers = false
-
-    # Run specs in random order to surface order dependencies. If you find an
-    # order dependency and want to debug it, you can fix the order by providing
-    # the seed, which is printed after each run.
-    #     --seed 1234
+    config.treat_symbols_as_metadata_keys_with_true_values = true
     config.order = "random"
+    # Filter specs to only run focus specs
+    config.filter_run :focus => true
+    # if none are focused, run everything
+    config.run_all_when_everything_filtered = true
+    # include the routes url_helpers
+    config.include Smithy::Engine.routes.url_helpers
+    config.include Rails.application.routes.url_helpers
   end
-
 end
 
 Spork.each_run do
