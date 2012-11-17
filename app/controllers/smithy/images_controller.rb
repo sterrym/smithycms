9679f646417beb84a2a15211d6935d224f0d7af7
Tim Glen <tim@tag.ca>
2012-11-17 09:14:07 -0500

    ImagesController

---
+++ app/controllers/smithy/images_controller.rb
@@ -0,0 +1,41 @@
+require_dependency "smithy/application_controller"
+
+module Smithy
+  class ImagesController < ApplicationController
+    respond_to :html, :json
+
+    def show
+      @image = Smithy::Image.find(params[:id])
+      respond_with @image
+    end
+
+    def new
+      @image = Smithy::Image.new(params[:image])
+      respond_with @image
+    end
+
+    def create
+      @image = Smithy::Image.new(params[:image])
+      @image.save
+      flash.notice = "Your image was created" if @image.persisted?
+      respond_with @image
+    end
+
+    def edit
+      @image = Smithy::Image.find(params[:id])
+      respond_with @image
+    end
+
+    def update
+      @image = Smithy::Image.find(params[:id])
+      flash.notice = "Your image was saved" if @image.update_attributes(params[:image])
+      respond_with @image
+    end
+
+    def destroy
+      @image = Smithy::Image.find(params[:id])
+      @image.destroy
+      respond_with @image
+    end
+  end
+end
