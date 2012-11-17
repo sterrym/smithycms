d4165783c92bb1be55b03ddba25ceee97bf58409
Tim Glen <tim@tag.ca>
2012-11-17 11:12:40 -0600

    added #to_liquid and .image_scaling_options for UI

--- app/models/smithy/image.rb
+++ app/models/smithy/image.rb
@@ -2,10 +2,25 @@ module Smithy
   class Image < ActiveRecord::Base
     include Smithy::ContentBlocks::Model
 
-    attr_accessible :alternate_text, :height, :image_scaling, :link_url, :width
+    attr_accessible :alternate_text, :asset_id, :height, :image_scaling, :link_url, :width
 
     validates_presence_of :asset
 
     belongs_to :asset
+
+    class << self
+      def image_scaling_options
+        [
+          ['Keep to Scale', ''],
+          ['Force Exact Dimensions (this may crop the image)', '#']
+        ]
+      end
+    end
+
+    def to_liquid
+      attributes.tap do |a|
+        a['asset'] = asset.to_liquid if asset.present?
+      end
+    end
   end
 end
