2ce5b9a0f62c2315797e0b6b278edec037a42ef8
Tim Glen <tim@tag.ca>
2012-11-26 12:45:05 -0500

    only load files whose tables exist

--- lib/smithy/engine.rb
+++ lib/smithy/engine.rb
@@ -8,7 +8,9 @@ module Smithy
     config.after_initialize do
       # we need to require all our model files so that ContentBlocks
       # are registered with the engine
-      Dir[File.join(File.dirname(__FILE__), '..', '..', 'app', 'models', 'smithy', '*.rb')].each {|file| require file }
+      Dir[File.join(File.dirname(__FILE__), '..', '..', 'app', 'models', 'smithy', '*.rb')].each do |file|
+        require file if ActiveRecord::Base.connection.table_exists?(file.pluralize)
+      end
     end
   end
 end
