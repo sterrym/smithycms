12a542b7b49bd6af240fe3853b833b684d780272
Tim Glen <tim@tag.ca>
2012-11-26 16:16:12 -0500

    trying this

--- lib/smithy/content_blocks/registry.rb
+++ lib/smithy/content_blocks/registry.rb
@@ -1,4 +1,5 @@
 require 'active_support/concern'
+require File.join(File.dirname(__FILE__), '..', '..', '..', 'app', 'models', 'smithy', 'content_block')
 
 module Smithy
   module ContentBlocks
@@ -15,7 +16,8 @@ module Smithy
         end
 
         def register(content_block, description = nil)
-          return unless ActiveRecord::Base.connection.table_exists?(Smithy::ContentBlock.table_name) && ActiveRecord::Base.connection.table_exists?(content_block.table_name)
+          return unless ActiveRecord::Base.connection.table_exists?(Smithy::ContentBlock.table_name)
+          return unless ActiveRecord::Base.connection.table_exists?(content_block.table_name)
           content_block_name = content_block.to_s.demodulize
           @@content_blocks << content_block_name unless @@content_blocks.include?(content_block_name)
           cb = Smithy::ContentBlock.find_or_initialize_by_name(content_block_name)
