af2afcf50c9f9e2d861c7f187e06be15845b2168
Tim Glen <tim@tag.ca>
2012-11-26 12:51:32 -0500

    removing the auto-require for now;

--- lib/smithy/engine.rb
+++ lib/smithy/engine.rb
@@ -8,9 +8,9 @@ module Smithy
     config.after_initialize do
       # we need to require all our model files so that ContentBlocks
       # are registered with the engine
-      Dir[File.join(File.dirname(__FILE__), '..', '..', 'app', 'models', 'smithy', '*.rb')].each do |file|
-        require file if ActiveRecord::Base.connection.table_exists?(file.pluralize)
-      end
+      # Dir[File.join(File.dirname(__FILE__), '..', '..', 'app', 'models', 'smithy', '*.rb')].each do |file|
+      #   require file if ActiveRecord::Base.connection.table_exists?(file.pluralize)
+      # end
     end
   end
 end
