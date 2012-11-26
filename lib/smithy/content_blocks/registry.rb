ef1f025bd54a698ac385e2b07e965d4852257cb1
Tim Glen <tim@tag.ca>
2012-11-26 12:38:13 -0500

    check for the content_blocks table first

--- lib/smithy/content_blocks/registry.rb
+++ lib/smithy/content_blocks/registry.rb
@@ -16,7 +16,7 @@ module Smithy
         end
 
         def register(content_block, description = nil)
-          return unless ActiveRecord::Base.connection.table_exists? content_block.table_name
+          return unless ActiveRecord::Base.connection.table_exists?(Smithy::ContentBlock.table_name) && ActiveRecord::Base.connection.table_exists?(content_block.table_name)
           content_block_name = content_block.to_s.demodulize
           @@content_blocks << content_block_name unless @@content_blocks.include?(content_block_name)
           cb = Smithy::ContentBlock.find_or_initialize_by_name(content_block_name)
