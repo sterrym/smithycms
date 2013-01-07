a3da9680c95106e72a308ea75613d683b6455c92
Tim Glen <tim@tag.ca>
2013-01-07 16:01:13 -0500

    Added authentication to Smithy. The application assumes that you must have a #smithy_user method and that user must have a #smithy_admin? boolean method available.

--- app/controllers/smithy/application_controller.rb
+++ app/controllers/smithy/application_controller.rb
@@ -1,4 +1,12 @@
-module Smithy
-  class ApplicationController < ActionController::Base
-  end
+class Smithy::ApplicationController < ApplicationController
+  before_filter :authenticate_smithy_admin
+
+  private
+
+    def authenticate_smithy_admin
+      if !smithy_user || !smithy_user.smithy_admin?
+        flash.alert = "You are not allowed to do that."
+        redirect_to "/" #TODO: not positive where to redirect here
+      end
+    end
 end
