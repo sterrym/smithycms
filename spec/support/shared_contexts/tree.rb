bfbf92c5bccc1d374079d534499d3d2ab9d005c6
Tim Glen <tim@tag.ca>
2013-01-23 15:49:11 -0500

    use new :publish acccessor

--- spec/support/shared_contexts/tree.rb
+++ spec/support/shared_contexts/tree.rb
@@ -1,13 +1,13 @@
 shared_context "a tree of pages" do
   let!(:home) { FactoryGirl.create(:page, :title => "Home") }
-  let!(:page1) { FactoryGirl.create(:page, :title => "Page 1", :parent => home) }
-  let!(:page1_1) { FactoryGirl.create(:page, :title => "Page 1-1", :parent => page1) }
-  let!(:page1_2) { FactoryGirl.create(:page, :title => "Page 1-2", :parent => page1) }
-  let!(:page1_3) { FactoryGirl.create(:page, :title => "Page 1-3", :parent => page1) }
-  let!(:page1_3_1) { FactoryGirl.create(:page, :title => "Page 1-3-1", :parent => page1_3) }
-  let!(:page2) { FactoryGirl.create(:page, :title => "Page 2", :parent => home) }
-  let!(:page2_1) { FactoryGirl.create(:page, :title => "Page 2-1", :parent => page2, :show_in_navigation => false) }
-  let!(:page2_2) { FactoryGirl.create(:page, :title => "Page 2-2", :parent => page2, :published_at => nil) }
-  let!(:page3) { FactoryGirl.create(:page, :title => "Page 3", :parent => home, :published_at => nil, :show_in_navigation => false) }
+  let!(:page1) { FactoryGirl.create(:page, :title => "Page 1", :parent => home, :publish => true) }
+  let!(:page1_1) { FactoryGirl.create(:page, :title => "Page 1-1", :parent => page1, :publish => true) }
+  let!(:page1_2) { FactoryGirl.create(:page, :title => "Page 1-2", :parent => page1, :publish => true) }
+  let!(:page1_3) { FactoryGirl.create(:page, :title => "Page 1-3", :parent => page1, :publish => true) }
+  let!(:page1_3_1) { FactoryGirl.create(:page, :title => "Page 1-3-1", :parent => page1_3, :publish => true) }
+  let!(:page2) { FactoryGirl.create(:page, :title => "Page 2", :parent => home, :publish => true) }
+  let!(:page2_1) { FactoryGirl.create(:page, :title => "Page 2-1", :parent => page2, :show_in_navigation => false, :publish => true) }
+  let!(:page2_2) { FactoryGirl.create(:page, :title => "Page 2-2", :parent => page2, :publish => false) }
+  let!(:page3) { FactoryGirl.create(:page, :title => "Page 3", :parent => home, :publish => false, :show_in_navigation => false) }
   let!(:page4) { FactoryGirl.create(:page, :title => "Page 4", :parent => home, :published_at => 1.day.from_now) }
 end
