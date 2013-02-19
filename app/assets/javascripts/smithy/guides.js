3372ae23db234d4ab8473ccd218042511d5b5b1d
Tim Glen <tim@tag.ca>
2013-02-19 11:31:21 -0500

    rewrote this since live() was removed from jquery

--- app/assets/javascripts/smithy/guides.js
+++ app/assets/javascripts/smithy/guides.js
@@ -1,25 +1,23 @@
-
-$('[data-toggle="remote-load"]').toggle(
-  function(e) {
-    e.preventDefault();
-    var activator = this;
-    var remote = $(this).attr("href");
-    var target = $(this).attr("data-target");
+$('[data-toggle="remote-load"]').click(function(e) {
+  e.preventDefault();
+  var activator = this;
+  var remote = $(this).attr("href");
+  var target = $(this).attr("data-target");
+  if ($(target).filter(":empty").length > 0) {
     $(target).hide();
-    $('[data-dismiss="remote-load"]', $(target)).live('click', function(e) {
-      e.preventDefault();
-      $(activator).click();
-    });
     $.ajax($(this).attr("href"), {
       success: function(data) {
         $(target).append($('<div data-type="remote-load"></div>').append(data));
+        $('[data-dismiss="remote-load"]', $(target)).on('click', function(e) {
+          e.preventDefault();
+          $(activator).click();
+        });
         $(target).slideDown('slow');
       }
     });
-  },
-  function(e) {
-    e.preventDefault();
-    target = $(this).attr("data-target");
-    $(target).slideUp('slow', function() { $(this).empty() })
+  } else if ($(target).filter(":hidden").length > 0) {
+    $(target).slideDown('slow');
+  } else if ($(target).filter(":visible").length > 0) {
+    $(target).slideUp('slow');
   }
-);
+});
