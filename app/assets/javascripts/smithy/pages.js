3491604cd63eca021733fe853e69f89467f38594
Tim Glen <tim@tag.ca>
2013-02-19 11:34:11 -0500

    removed live(), added on()

--- app/assets/javascripts/smithy/pages.js
+++ app/assets/javascripts/smithy/pages.js
@@ -1,19 +1,15 @@
-$(document).ready(function() {
-
+$(function() {
   // Support for AJAX loaded modal window.
   // Focuses on first input textbox after it loads the window.
-  $('[data-toggle="modal"]').live('click', function(e) {
+  $("[data-toggle='modal']").on('click', function(e) {
     e.preventDefault();
     var url = $(this).attr('href');
     if (url.indexOf('#') == 0) {
       $(url).modal('open');
     } else {
-      console.log(url);
       $.get(url, function(data) {
-        console.log(data);
         $('<div class="modal hide fade">' + data + '</div>').modal();
       }).success(function() { $('input:text:visible:first').focus(); });
     }
   });
-
 });
