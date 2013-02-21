f415335cd681e65140452be59a1a6fd6eb76ab76
Tim Glen <tim@tag.ca>
2013-02-21 10:48:40 -0500

    redirect if the page is an external link

--- app/controllers/smithy/pages_controller.rb
+++ app/controllers/smithy/pages_controller.rb
@@ -17,6 +17,7 @@ module Smithy
     def show
       params[:path] = '' if params[:id].nil? && params[:path].nil? # sets the root path when nothing else is passed
       @page = Page.find(params[:path].nil? ? params[:id] : "/#{params[:path]}")
+      redirect_to @page.external_link and return if @page.external_link?
       # TODO: more exploration is needed around caching. Some server-side caching as well, for instance
       expires_in(@page.cache_length.to_i.seconds)
       if stale?(@page)
