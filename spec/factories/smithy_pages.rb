05b4546bceb6d11945d7a75a9e626c90df3d471b
Tim Glen <tim@tag.ca>
2013-03-12 16:35:09 -0400

    added description and keywords to page factory

--- spec/factories/smithy_pages.rb
+++ spec/factories/smithy_pages.rb
@@ -4,6 +4,8 @@ FactoryGirl.define do
   factory :page, :class => 'Smithy::Page' do
     template
     title { Faker::Lorem.words(2).join(' ') }
+    description { Faker::Lorem.sentence }
+    keywords { Faker::Lorem.words(10).join(' ') }
     published_at 1.day.ago
   end
 end
