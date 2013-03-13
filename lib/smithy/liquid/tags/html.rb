5ce165800bc11e6cb7c29455b9cb91642e3cb88b
Tim Glen <tim@tag.ca>
2013-03-13 15:27:05 -0400

    added smithy_javascript_include_tag and smithy_stylesheet_link_tag for internal js and css templates

--- lib/smithy/liquid/tags/html.rb
+++ lib/smithy/liquid/tags/html.rb
@@ -34,9 +34,46 @@ module Smithy
             controller.view_context.send(:javascript_include_tag, @javascript)
           end
         end
+
+        class SmithyJavascriptIncludeTag < ::Liquid::Tag
+          Syntax = /(#{::Liquid::Expression}+)?/
+          def initialize(tag_name, markup, tokens)
+            if markup =~ Syntax
+              @javascript = $1.gsub('\'', '')
+              @javascript = "#{@javascript}.js" unless @javascript.match(/\.js$/)
+            else
+              raise ::Liquid::SyntaxError.new("Syntax Error in 'smithy_javascript_include_tag' - Valid syntax: smithy_javascript_include_tag <javascript>")
+            end
+            super
+          end
+
+          def render(context)
+            controller  = context.registers[:controller]
+            controller.view_context.send(:javascript_include_tag, "/templates/javascripts/#{@javascript}")
+          end
+        end
+        class SmithyStylesheetLinkTag < ::Liquid::Tag
+          Syntax = /(#{::Liquid::Expression}+)?/
+          def initialize(tag_name, markup, tokens)
+            if markup =~ Syntax
+              @stylesheet = $1.gsub('\'', '')
+              @stylesheet = "#{@stylesheet}.css" unless @stylesheet.match(/\.css$/)
+            else
+              raise ::Liquid::SyntaxError.new("Syntax Error in 'stylesheet_link_tag' - Valid syntax: stylesheet_link_tag <stylesheet>")
+            end
+            super
+          end
+
+          def render(context)
+            controller  = context.registers[:controller]
+            controller.view_context.send(:stylesheet_link_tag, "/templates/stylesheets/#{@stylesheet}")
+          end
+        end
       end
       ::Liquid::Template.register_tag('stylesheet_link_tag', Html::StylesheetLinkTag)
       ::Liquid::Template.register_tag('javascript_include_tag', Html::JavascriptIncludeTag)
+      ::Liquid::Template.register_tag('smithy_javascript_include_tag', Html::SmithyJavascriptIncludeTag)
+      ::Liquid::Template.register_tag('smithy_stylesheet_link_tag', Html::SmithyStylesheetLinkTag)
     end
   end
 end
--- spec/lib/smithy/liquid/tags/html_spec.rb
+++ spec/lib/smithy/liquid/tags/html_spec.rb
@@ -11,6 +11,26 @@ describe Smithy::Liquid::Tags::Html do
     html.should eql '<script src="/assets/foo.js" type="text/javascript"></script>'
   end
 
+  it 'renders the smithy javascript include tag' do
+    html = render_tag('smithy_javascript_include_tag', 'foo')
+    html.should eql '<script src="/templates/javascripts/foo.js" type="text/javascript"></script>'
+  end
+
+  it 'doesn\'t add ".css" if it\'s already there' do
+    html = render_tag('smithy_javascript_include_tag', 'foo.js')
+    html.should eql '<script src="/templates/javascripts/foo.js" type="text/javascript"></script>'
+  end
+
+  it 'renders the smithy stylesheet link tag' do
+    html = render_tag('smithy_stylesheet_link_tag', 'foo')
+    html.should eql '<link href="/templates/stylesheets/foo.css" media="screen" rel="stylesheet" type="text/css" />'
+  end
+
+  it 'doesn\'t add ".css" if it\'s already there' do
+    html = render_tag('smithy_stylesheet_link_tag', 'foo.css')
+    html.should eql '<link href="/templates/stylesheets/foo.css" media="screen" rel="stylesheet" type="text/css" />'
+  end
+
   def render_tag(tag_name, param)
     controller = ApplicationController.new
     registers       = { :controller => controller }
