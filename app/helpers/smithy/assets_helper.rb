4059744da7107392b47bf1a4d6dc4281127faa19
Tim Glen <tim@tag.ca>
2013-03-13 11:10:17 -0400

    add the local host to asset images

--- app/helpers/smithy/assets_helper.rb
+++ app/helpers/smithy/assets_helper.rb
@@ -3,7 +3,7 @@ module Smithy
     def asset_image_tag(asset_id)
       asset = Smithy::Asset.find_by_id(asset_id)
       return unless asset
-      image_tag(asset.file.url, :alt => asset.name)
+      image_tag(asset.file.url(:host => "#{request.protocol}#{request.host_with_port}"), :alt => asset.name)
     end
   end
 end
