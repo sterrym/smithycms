471cd428339d67d8e7a02e211de02029534c7e3f
Tim Glen <tim@tag.ca>
2013-03-14 17:34:41 -0400

    stale? needs public too

--- app/controllers/smithy/pages_controller.rb
+++ app/controllers/smithy/pages_controller.rb
@@ -20,7 +20,7 @@ module Smithy
       redirect_to @page.external_link and return if @page.external_link?
       # adding :public param allow Rack::Cache to cache the result
       expires_in(@page.cache_length.to_i.seconds, :public => true)
-      if stale?(@page)
+      if stale?(@page, :public => true)
         respond_with @page do |format|
           format.html {
             render_smithy_page
