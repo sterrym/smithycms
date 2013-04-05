98fd050bd03905e7cf841257116ce0686b13ae62
Tim Glen <tim@tag.ca>
2013-04-05 15:10:50 -0400

    fixed the layout

--- app/controllers/smithy/base_controller.rb
+++ app/controllers/smithy/base_controller.rb
@@ -1,10 +1,12 @@
 class Smithy::BaseController < ApplicationController
+  layout 'smithy/application'
+
   before_filter :authenticate_smithy_admin
 
   private
 
     def authenticate_smithy_admin
-      if !smithy_user || !smithy_user.smithy_admin?
+      if !smithy_current_user || !smithy_current_user.smithy_admin?
         flash.alert = "You are not allowed to do that."
         redirect_to smithy_login_path
       end
