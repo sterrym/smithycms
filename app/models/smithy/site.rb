442002e73eff240740aef03ea52609a78e599b82
Tim Glen <tim@tag.ca>
2013-04-15 10:58:12 -0400

    added a site title

--- app/models/smithy/site.rb
+++ app/models/smithy/site.rb
@@ -1,5 +1,20 @@
 module Smithy
   class Site
+    @@title = nil
+
+    class << self
+      def title
+        @@title
+      end
+
+      def title=(str)
+        @@title = str
+      end
+    end
+
+    def title
+      self.class.title
+    end
 
     def root
       @root ||= Smithy::Page.root
@@ -7,6 +22,7 @@ module Smithy
 
     def to_liquid
       {
+        'title' => self.title,
         'root' => self.root.to_liquid
       }
     end
--- spec/models/smithy/site_spec.rb
+++ spec/models/smithy/site_spec.rb
@@ -5,7 +5,16 @@ describe Smithy::Site do
 
   subject { Smithy::Site.new }
 
-  its(:root) { should == home }
+  its(:root) { should eql home }
+  its(:title) { should be_nil }
+
+  context "with a title" do
+    before do
+      Smithy::Site.title = 'FooBar'
+    end
+    its(:title) { should eql 'FooBar' }
+  end
+
   describe "#to_liquid" do
     specify "calls to_liquid on the root" do
       subject.root.should_receive(:to_liquid)
