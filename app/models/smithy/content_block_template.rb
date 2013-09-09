75f51f278745e3db5e877d28874374fad9edc645
Tim Glen <tim@tag.ca>
2013-09-09 16:00:50 -0400

    expire page caches on update

--- app/models/smithy/content_block_template.rb
+++ app/models/smithy/content_block_template.rb
@@ -2,16 +2,25 @@ module Smithy
   class ContentBlockTemplate < ActiveRecord::Base
     attr_accessible :content, :name
 
-    belongs_to :content_block
+    belongs_to :content_block, :touch => true
+    has_many :page_contents
 
     validates_presence_of :name
     validates_uniqueness_of :name, :scope => :content_block_id
     validates_presence_of :content
+
+    after_save :touch_page_contents
 
     default_scope order(:name)
 
     def liquid_template
       @liquid_template ||= ::Liquid::Template.parse(self.content)
     end
+
+    private
+      def touch_page_contents
+        self.page_contents.each(&:touch)
+      end
+
   end
 end
