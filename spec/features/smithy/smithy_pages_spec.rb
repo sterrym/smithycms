ec100148b2352e9416edf9d79a4ccbf57e6979ba
Tim Glen <tim@tag.ca>
2013-11-11 14:06:43 -0500

    Added a spec to test basic HTML template and content rendering

---
+++ spec/features/smithy/smithy_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "Page" do
+  let(:template) { FactoryGirl.create(:template, :content => File.read(Smithy::Engine.root.join('spec', 'fixtures', 'templates', 'foo.html.liquid'))) }
+  let(:home) { FactoryGirl.create(:page, :title => "This is a Page", :template => template) }
+  let(:content_block) { Smithy::ContentBlock.find_by_name("Content") }
+  let(:content_block_template) { FactoryGirl.create(:content_block_template, :content => "{{ content }}", :content_block => content_block)}
+  let(:content) { FactoryGirl.create(:content, :content => "This is some sample content.") }
+  let!(:page_content) { FactoryGirl.create(:page_content, :page => home, :container => 'foo', :content_block => content, :content_block_template => content_block_template) }
+
+  it "should render the Template" do
+    visit '/'
+    expect(page).to have_selector 'html'
+    expect(page).to have_selector 'body'
+  end
+
+  it "should render the Content" do
+    visit '/'
+    expect(page).to have_content 'This is some sample content.'
+  end
+end
