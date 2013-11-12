e4021109414292ccce70815cf6614a9ec6502b6a
Tim Glen <tim@tag.ca>
2013-11-12 10:14:47 -0500

    better migration with data update

--- db/migrate/20131003210228_add_publishable_to_smithy_page_contents.rb
+++ db/migrate/20131003210228_add_publishable_to_smithy_page_contents.rb
@@ -1,5 +1,11 @@
 class AddPublishableToSmithyPageContents < ActiveRecord::Migration
-  def change
+  def up
     add_column :smithy_page_contents, :publishable, :boolean, :default => false
+    Smithy::PageContent.reset_column_information
+    Smithy::PageContent.all.each{|pc| pc.update_attribute(:publishable, true) }
+  end
+
+  def down
+    remove_column :smithy_page_contents, :publishable
   end
 end
--- spec/dummy/db/migrate/20131110200252_add_publishable_to_smithy_page_contents.smithy.rb
+++ spec/dummy/db/migrate/20131110200252_add_publishable_to_smithy_page_contents.smithy.rb
@@ -1,6 +1,12 @@
 # This migration comes from smithy (originally 20131003210228)
 class AddPublishableToSmithyPageContents < ActiveRecord::Migration
-  def change
+  def up
     add_column :smithy_page_contents, :publishable, :boolean, :default => false
+    Smithy::PageContent.reset_column_information
+    Smithy::PageContent.all.each{|pc| pc.update_attribute(:publishable, true) }
+  end
+
+  def down
+    remove_column :smithy_page_contents, :publishable
   end
 end
