65bb5d77319197d2a3d41dfc1c2836b2807c3e7e
Tim Glen <tim@tag.ca>
2014-05-09 14:06:07 -0400

    Added a link_to block tag

---
+++ lib/smithy/liquid/tags/link.rb
@@ -0,0 +1,33 @@
+module Smithy
+  module Liquid
+    module Tags
+      class LinkTo < ::Liquid::Block
+        SyntaxHelp = "Syntax Error in tag 'link_to' - Valid syntax: link_to [url]"
+        Syntax = /(#{::Liquid::QuotedFragment})\s*([=!<>a-z_]+)?\s*(#{::Liquid::QuotedFragment})?/o
+
+        def initialize(tag_name, markup, tokens)
+          @tokens = tokens
+          @url = markup.strip
+          @url = :back if @url == ':back'
+          super
+        end
+
+        def render(context)
+          debugger
+          controller = context.registers[:controller]
+          context.stack do
+            controller.view_context.link_to(@url) do
+              render_all(nodes, context)
+            end
+          end
+        end
+
+        private
+          def nodes
+            @nodelist.map{|n| n.is_a?(String) ? n.strip : n }
+          end
+      end
+      ::Liquid::Template.register_tag('link_to', Smithy::Liquid::Tags::LinkTo)
+    end
+  end
+end
