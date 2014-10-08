3a4897c01401c5efd105440c7fee739a2a91e878
Tim Glen <tim@tag.ca>
2014-10-08 17:18:24 -0400

    Updated to RSpec.current_example

--- spec/support/dependencies.rb
+++ spec/support/dependencies.rb
@@ -9,20 +9,20 @@ require 'fuubar'
 require 'launchy'
 require 'letter_opener'
 require 'rack-livereload'
-require 'rspec-rails'
 require 'shoulda-matchers'
 require 'smithycms-auth'
 
 Fog.mock! # this mocks out all AWS calls - really nice
 
 RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
   # configure Database cleaning
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
   config.before(:each) do |group|
-    DatabaseCleaner.strategy = group.example.metadata[:type] == :feature ? :truncation : :transaction
+    DatabaseCleaner.strategy = RSpec.current_example.metadata[:type] == :feature ? :truncation : :transaction
     DatabaseCleaner.start
   end
   config.after(:each) do
