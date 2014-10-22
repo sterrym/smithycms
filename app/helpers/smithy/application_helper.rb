622012b7322452114253caa10f0c49312f3d25b1
Tim Glen <tim@tag.ca>
2014-10-22 10:58:21 -0400

    The name was causing interference. Very naughty

--- app/helpers/smithy/application_helper.rb
+++ app/helpers/smithy/application_helper.rb
@@ -1,6 +1,6 @@
 module Smithy
   module ApplicationHelper
-    def link_to_add_fields(name, association, form_builder)
+    def smithy_link_to_add_fields(name, association, form_builder)
       html = content_tag(:div, :id => "#{association}_fields_blueprint", :style => "display:none;") do
         form_builder.semantic_fields_for(association, model_object(form_builder, association), :child_index => "new_#{association}") do |builder|
           form_builder.template.concat(raw render("#{form_builder.object.class.to_s.tableize}/#{association.to_s.singularize}_fields", :f => builder))
@@ -10,7 +10,7 @@ module Smithy
       form_builder.template.link_to(name, "javascript:void(0)", :class => "add_nested_fields", "data-association" => association)
     end
 
-    def link_to_remove_fields(name, form_builder)
+    def smithy_link_to_remove_fields(name, form_builder)
       form_builder.hidden_field(:_destroy) + @template.link_to(name, "javascript:void(0)", :class => "remove_nested_fields")
     end
 
