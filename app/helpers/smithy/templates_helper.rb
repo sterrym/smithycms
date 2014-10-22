a46cbfe5a3c728a97f96d1a61ba87590467499b4
Tim Glen <tim@tag.ca>
2014-10-22 10:27:05 -0400

    Added some helpers to allow Smithy to play more nicely with others

--- app/helpers/smithy/templates_helper.rb
+++ app/helpers/smithy/templates_helper.rb
@@ -1,4 +1,16 @@
 module Smithy
   module TemplatesHelper
+    include Smithy::Liquid::Rendering
+
+    def render_smithy_nav(options="")
+      context = ::Liquid::Context.new({}, smithy_default_assigns, smithy_default_registers, false)
+      ::Liquid::Template.parse("{% nav #{options} %}").render(liquid_context)
+    end
+
+    def render_smithy_partial(partial_name)
+      partial_template = Smithy::Template.partials.find_by_name(partial_name)
+      return if partial_template.blank?
+      partial_template.liquid_template.render(liquid_context)
+    end
   end
 end
