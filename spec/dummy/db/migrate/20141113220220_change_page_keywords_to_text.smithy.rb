ed543ed0091bf57ff20bf2cee3bd3801d302866d
Tim Glen <tim@tag.ca>
2014-11-14 16:57:58 -0500

    dummy files

---
+++ spec/dummy/db/migrate/20141113220220_change_page_keywords_to_text.smithy.rb
@@ -0,0 +1,5 @@
+# This migration comes from smithy (originally 20141113220013)
+class ChangePageKeywordsToText < ActiveRecord::Migration
+  def change
+  end
+end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124164812) do
+ActiveRecord::Schema.define(version: 20141113220220) do
 
   create_table "smithy_assets", force: true do |t|
     t.string   "name"
