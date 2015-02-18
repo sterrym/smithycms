5cc1c91352c9021e4a97c7a648dd5d817430968b
Tim Glen <tim@tag.ca>
2015-02-18 10:32:42 -0500

    some minor adjustments to the editor

--- app/assets/javascripts/smithy/editor.js
+++ app/assets/javascripts/smithy/editor.js
@@ -1,17 +1,19 @@
 var ace_edit = function(id, template_type, name) {
   if (!name) { name = 'template_content' }
-  var editor_name = name + '_editor-' + id
-  $('#'+editor_name).show();
-  var editor = ace.edit(editor_name);
-  var textarea = $('textarea[id="' + name + '-'+id+'"]').hide();
+  var textarea_id = name + '-' + id
+  var editor_id = name + '_editor-' + id
+  $('#'+editor_id).show();
+  var editor = ace.edit(editor_id);
+  var $textarea = $('#'+textarea_id);
+  $textarea.hide()
   var session = editor.getSession();
   session.setMode("ace/mode/" + template_type);
-  session.setValue(textarea.val());
+  session.setValue($textarea.val());
   session.setTabSize(2);
   session.setUseSoftTabs(true);
   session.setUseWrapMode(true);
   session.on('change', function(){
-    textarea.val(editor.getSession().getValue());
+    $textarea.val(editor.getSession().getValue());
   });
   editor.commands.addCommand({
     name: 'save',
