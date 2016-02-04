69104be9835d6da6fcda06bb1d5b89dd448dd4a9
Daniel Robinson <d.robinson004@gmail.com>
2016-02-03 21:16:32 -0500

    oops shouldn't have committed my aws key...

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -7,16 +7,6 @@ Rails.application.routes.prepend do
   mount Refile.app, at: Refile.mount_point, as: :refile_app
 end
 
-# aws = {
-#   access_key_id: 'AKIAJTMAZVFVSBOL6AKQ',
-#   secret_access_key: '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',
-#   region: 'us-west-2',
-#   bucket: 'testsmithy',
-# }
-
-# Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-# Refile.store = Refile::S3.new(prefix: "store", **aws)
-
 if ENV['AWS_ACCESS_KEY_ID'].present? && ENV['AWS_SECRET_ACCESS_KEY'].present? && ENV['AWS_S3_BUCKET'].present?
   aws = {
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
