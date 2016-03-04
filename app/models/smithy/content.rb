2da1ffc40a97569f8e1f5b072fdecc67047c3f01
Tim Glen <tim@tagstudios.io>
2016-03-04 13:42:20 -0500

    Might as well force to a string at the source

--- app/models/smithy/content.rb
+++ app/models/smithy/content.rb
@@ -19,14 +19,14 @@ module Smithy
     def to_liquid
       {
         'id' => self.id,
-        'content' => Smithy::AssetLink.fix(self.content.to_s),
+        'content' => Smithy::AssetLink.fix(self.content),
         'formatted_content' => self.formatted_content
       }
     end
 
     private
       def render_markdown_content
-        formatter = Smithy::Formatter.new(self.content.to_s)
+        formatter = Smithy::Formatter.new(self.content)
         self.markdown_content = formatter.render
       end
   end
--- lib/smithy/asset_link.rb
+++ lib/smithy/asset_link.rb
@@ -1,7 +1,7 @@
 module Smithy
   class AssetLink
     def self.fix(content)
-      content.gsub(/\/smithy\/assets\/([0-9]+)/) { Smithy::Asset.find($1).url }
+      content.to_s.gsub(/\/smithy\/assets\/([0-9]+)/) { Smithy::Asset.find($1).url }
     end
   end
 end
