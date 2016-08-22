24cd48f6e4da6a4a68712b3ff57ecc2b15c326f4
Tim Glen <tim@tagstudios.io>
2016-08-22 10:45:40 -0400

    Added a page_content_wrapper

---
+++ lib/smithy/page_content_wrapper.rb
@@ -0,0 +1,14 @@
+module Smithy
+  class PageContentWrapper
+    attr_reader :page_content
+    include ActionView::Helpers::TagHelper
+
+    def initialize(page_content)
+      @page_content = page_content
+    end
+
+    def wrap(output)
+      content_tag(:div, output.html_safe, id: "page_content_#{page_content.id}", class: page_content.css_classes)
+    end
+  end
+end
