8342b500aa268a2d24bd26f1010407d316c6e24e
Tim Glen <tim@tagstudios.io>
2016-08-25 10:54:33 -0400

    Not working, but sets a better direction for the specs. More readable, parseable

--- spec/models/smithy/permitted_params_spec.rb
+++ spec/models/smithy/permitted_params_spec.rb
@@ -15,35 +15,56 @@ RSpec.describe Smithy::PermittedParams, :type => :model do
     expect(permitted_params.params_for(:page)).to eq(params[:page].with_indifferent_access)
   end
 
-  {
-    asset: [ :name, :file, :file_name, :file_url, :retained_file, :uploaded_file_url ],
-    content: [ :content ],
-    content_block: :all,
-    content_block_template:  [ :content, :name ],
-    image: [ :alternate_text, :asset_id, :height, :html_attributes, :image_scaling, :link_url, :width, :content ],
-    page:  [ :browser_title, :cache_length, :description, :external_link, :keywords, :permalink, :publish, :published_at, :show_in_navigation, :title, :parent_id, :template_id, :copy_content_from],
-    page_content: :all,
-    page_list: [ :count, :page_template_id, :parent_id, :include_children, :sort ],
-    setting: [ :name, :value ],
-    template:  [ :name, :content, :template_type ],
-    template_container:  [ :name, :position ]
-  }.each do |param_name, columns|
-    context "for #{param_name}" do
-      let(:param) { param_name.to_sym }
-      let(:attributes) { fetch_attributes param }
-      let(:params) { ActionController::Parameters.new( param => attributes ) }
-      describe "##{param_name}_attributes" do
-        subject { permitted_params.send("#{param_name}_attributes") }
-        it { is_expected.to eq(columns) }
-      end
-      describe "#params_for(:#{param_name})" do
-        subject { permitted_params.params_for(param) }
-        it { is_expected.to eq(attributes) }
-      end
+  shared_examples "specific permitted_params" do |param_name, allowed_attributes|
+    let(:param_name) { param_name }
+    let(:allowed_attributes) { allowed_attributes }
+    let(:param) { param_name.to_sym }
+    let(:attributes) { fetch_attributes param }
+    describe "##{param_name}_attributes" do
+      subject { permitted_params.send("#{param_name}_attributes") }
+      it { is_expected.to eq(allowed_attributes) }
+    end
+    describe "#params_for(:#{param_name})" do
+      subject { permitted_params.params_for(param) }
+      it { is_expected.to eq(attributes) }
+    end
+    def fetch_attributes(model)
+      ActionController::Parameters.new(attributes_for(model))
     end
   end
 
-  def fetch_attributes(model)
-    ActionController::Parameters.new(attributes_for(model))
+  describe "asset" do
+    it_should_behave_like "specific permitted_params", "asset", [ :name, :file, :file_name, :file_url, :retained_file, :uploaded_file_url ]
   end
+  describe "content" do
+    it_should_behave_like "specific permitted_params", "content", [ :content ]
+  end
+  describe "content_block" do
+    it_should_behave_like "specific permitted_params", "content_block", [:name, {:templates_attributes=>[:id, :name, :content, :_destroy]}]
+  end
+  # describe "content_block_template" do
+  #   it_should_behave_like "specific permitted_params", "content_block_template",  [ :name, :content  ]
+  # end
+  # describe "image" do
+  #   it_should_behave_like "specific permitted_params", "image", [ :alternate_text, :asset_id, :height, :html_attributes, :image_scaling, :link_url, :width, :content ]
+  # end
+  # describe "page" do
+  #   it_should_behave_like "specific permitted_params", "page", [ :browser_title, :cache_length, :description, :external_link, :keywords, :permalink, :publish, :published_at, :show_in_navigation, :title, :parent_id, :template_id, :copy_content_from]
+  # end
+  # describe "page_content" do
+  #   it_should_behave_like "specific permitted_params", "page_content", :all
+  # end
+  # describe "page_list" do
+  #   it_should_behave_like "specific permitted_params", "page_list", [ :count, :page_template_id, :parent_id, :include_children, :sort ]
+  # end
+  # describe "setting" do
+  #   it_should_behave_like "specific permitted_params", "setting", [ :name, :value ]
+  # end
+  # describe "template" do
+  #   it_should_behave_like "specific permitted_params", "template",  [ :name, :content, :template_type ]
+  # end
+  # describe "template_container" do
+  #   it_should_behave_like "specific permitted_params", "template_container", [ :name, :position ]
+  # end
+
 end
