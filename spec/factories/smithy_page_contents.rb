bdfec8a32a003959d7ec7c7f56b19d48bea3c93b
Tim Glen <tim@tagstudios.io>
2016-09-07 11:37:34 -0400

    Added the content_block_template association

--- spec/factories/smithy_page_contents.rb
+++ spec/factories/smithy_page_contents.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
     container 'main_content'
     label { FFaker::Lorem.words(2).join(' ') }
     association :content_block, factory: :content
+    association :content_block_template
     trait :publishable do
       publishable true
     end
